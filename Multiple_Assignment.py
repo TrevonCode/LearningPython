e3e9db98aaf5f5e0df45af5417abd6f381ac6ce8
TrevonCode <109678376+TrevonCode@users.noreply.github.com>
2022-07-22 12:17:43 -0400

    Add files via upload

---
+++ Multiple_Assignment.py
@@ -0,0 +1,23 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+# name = "Trey"
+# age = 25
+# attractive = True
+
+#name, age, attractive = "Trey", 25, True
+
+#print(name)
+#print(age)
+#print(attractive)
+
+# Spongebob = 30
+# Sandy = 30
+# Patrick = 30
+# Squidward = 30
+
+Spongebob = Sandy = Patrick = Squidward = 30
+
+print(Spongebob)
+print(Sandy)
+print(Patrick)
+print(Squidward)
