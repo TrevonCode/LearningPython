da4c142296a0df5d2b47ab98b4aa70008d40503a
TrevonCode <109678376+TrevonCode@users.noreply.github.com>
2022-07-22 12:16:08 -0400

    Add files via upload

---
+++ User_Input.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = input(("How tall are you (in inches)?: "))
+
+#age = age + 1
+
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+print("You are "+str(height)+" inches tall")
